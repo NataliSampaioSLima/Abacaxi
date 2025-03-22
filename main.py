b4829d16287aa0ecc11da1b78708f98d0737ad24
NataliSampaioSLima <dev.natali.sampaio@gmail.com>
2025-03-22 10:05:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ async def root():
     return {"message": "Hello word"}
 
 #portas de entrada diferentes 127.0.0.1:8000/teste1
-@app.get("/teste1")
+@app.get("/teste")
 async def funcaoteste():
     return{"teste":True,"numaleatorio": random.randint(0,1000)}
